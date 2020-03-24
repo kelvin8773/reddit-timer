1cb2f8d6252706acf7704f0c1f88ca8509f23718
Kelvin Liang <kelvin9877@gmail.com>
2020-03-24 18:51:21 +0800

    start to work on the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './stylesheets/index.scss';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
