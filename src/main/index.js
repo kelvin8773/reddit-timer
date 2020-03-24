42e756c5e094acbb06af1156ca3aad99380ccaac
Kelvin Liang <kelvin9877@gmail.com>
2020-03-24 20:13:47 +0800

    try to fix route

--- src/main/index.js
+++ src/main/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {
-  Switch,
-  Route,
+    Switch,
+    Route,
 } from 'react-router-dom';
 
+
+import Home from './home';
 import Search from './search';
 
 const Main = () => (
-  <div className="Main text-center">
-    <Switch>
-      <Route path="/">
-        Home Content Here. (learn React)
-      </Route>
+    <div className="Main text-center">
+        <Switch>
+            <Route path="/">
+                <Home />
+            </Route>
 
-      <Route path="/search">
-        <Search />
-      </Route>
-    </Switch>
-  </div>
+            <Route path="/search">
+                <Search />
+            </Route>
+        </Switch>
+    </div>
 );
 
 export default Main;
