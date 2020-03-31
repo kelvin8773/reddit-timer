bceebe94f32d3ab7556a3a8eb4236e1c1e2dfb67
Kelvin Liang <kelvin9877@gmail.com>
2020-03-31 15:34:43 +0800

    add footer links and styles properly

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+
+import styles from './footer.module.scss';
+import { ReactComponent as Logo } from './sign.svg';
 
 const Footer = () => (
-  <div className="Footer text-center">
-    Footer
+  <div className={styles.footer}>
+    <a href=" https://ooloo.io" className={styles.weblink}>
+      ooloo.io
+    </a>
+    <Link to="/" className={styles.logolink}>
+      <Logo />
+    </Link>
+    <Link to="/terms">
+      Terms & Privacy
+    </Link>
   </div>
+
 );
 
 export default Footer;
