226675a1ba9dfcaec195d9d90b39b3cf9362609f
Kelvin Liang <kelvin9877@gmail.com>
2020-04-02 15:10:35 +0800

    finish setup for info section

--- src/main/home.js
+++ src/main/home.js
@@ -2,11 +2,21 @@ import React from 'react';
 import styles from './home.module.scss';
 
 const Home = () => (
-  <div>
+  <div className={styles.home}>
     <div id="how-it-works" className={styles.howItWorks}>
       <h3>
         How it works
       </h3>
+
+      <p>
+        • We find the 500 top posts from the past year for a subreddit.
+      </p>
+      <p>
+        • The data is visualized in a heatmap grouped by weekday and hour of the day.
+      </p>
+      <p>
+        • See immediately when to submit your reddit post.
+      </p>
 
     </div>
 
@@ -14,6 +24,15 @@ const Home = () => (
       <h3>
         About
       </h3>
+
+      <p>
+        This app was created during a course on
+        <a href=" https://ooloo.io" className={styles.link}> ooloo.io </a>
+        with the goal to implement a pixel-perfect real-world application with professional workflows and tools like Kanban, Asana, Zeplin, GitHub, pull requests and code reviews.&nbsp;
+        <a href="https://ooloo.io/employers" className={styles.link}>
+          Click here for more information.
+        </a>
+      </p>
 
     </div>
 
