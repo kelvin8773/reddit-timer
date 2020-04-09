88ed45df1cef6a3899a923f21ab40c903de76ce8
Kelvin Liang <kelvin9877@gmail.com>
2020-04-09 16:15:12 +0800

    linter fix

--- src/main/components/heroSection.js
+++ src/main/components/heroSection.js
@@ -1,46 +1,47 @@
 import React from 'react';
 import {
-    Link,
+  Link,
 } from 'react-router-dom';
 import Button from './button';
 import styles from './hero.module.scss';
 import heatMap from './table.png';
 import searchJson from '../../config/search.json';
 
-const destinatin = `/search?${searchJson.default}`
+const destinatin = `/search?${searchJson.default}`;
 
 const HeroSection = () => (
-    <div id="hero-section" className={styles.hero}>
-        <h1 className={styles.heroTitle}>
-            No reactions to your reddit posts?
-         </h1>
+  <div id="hero-section" className={styles.hero}>
+    <h1 className={styles.heroTitle}>
+      No reactions to your reddit posts?
+    </h1>
 
-        <h4 className={styles.subTitle}>
-            Great timing, great results! Find the best time to post on your subreddit.
-        </h4>
+    <h4 className={styles.subTitle}>
+      Great timing, great results! Find the best time to post on your subreddit.
+    </h4>
 
-        <div className={styles.heroButton}>
-            <Link to={destinatin} >
-                <Button>
-                    Show me the best time
-                </Button>
-            </Link>
-        </div>
+    <div className={styles.heroButton}>
+      <Link to={destinatin}>
+        <Button>
+          Show me the best time
+        </Button>
+      </Link>
+    </div>
 
 
-        <div className={styles.searchValue}>
-            r/{searchJson.default}
-        </div>
+    <div className={styles.searchValue}>
+      r/
+      {searchJson.default}
+    </div>
 
-        <Link to={destinatin} >
-            <img
-                src={heatMap}
-                alt="heat map"
-                className={styles.heatMap}
-            />
-        </Link>
+    <Link to={destinatin}>
+      <img
+        src={heatMap}
+        alt="heat map"
+        className={styles.heatMap}
+      />
+    </Link>
 
-    </div >
+  </div>
 );
 
 export default HeroSection;
