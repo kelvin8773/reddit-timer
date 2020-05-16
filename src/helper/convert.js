3580b67f42610bc3315a4b584a5ef55818094ea0
Kelvin Liang <kelvin9877@gmail.com>
2020-05-16 19:48:36 +0800

    linter fix

--- src/helper/convert.js
+++ src/helper/convert.js
@@ -2,4 +2,4 @@ export const getIndex = (weekday, hour) => weekday * 100 + hour;
 
 export const getDay = (index) => Math.floor(index / 100);
 
-export const getHour = index => index % 100;
+export const getHour = (index) => index % 100;
